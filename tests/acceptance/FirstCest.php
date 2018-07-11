4d473b5ded8eca907dde3742ab0ac883727210ba
jltalusan <johnlimueltalusan@gmail.com>
2018-07-11 09:58:14 +0800

    Updating FirstCest.php

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -15,20 +15,17 @@ class FirstCest
     public function tryToTest(AcceptanceTester $I)
     {
         $I->amOnPage('/qaengineers/');
+        $I->click('JLST');
+        $I->seeCurrentURLEquals('/qaengineers/');
         $I->click('About');
         $I->seeCurrentURLEquals('/qaengineers/about');
         $I->click('Posts');
         $I->seeCurrentURLEquals('/qaengineers/');
-        $I->click('JLST');
-        $I->seeCurrentURLEquals('/qaengineers/');
-        $I->click('//a[@class="catalogue-item"][1]/div/h1[@class="catalogue-title"]');
-        $I->seeCurrentURLEquals('/qaengineers/2018-06-29/How-to-install-Centos7minimal-in-VirtualBox');
-        $I->see('Introduction');
-        $I->see('What is Centos 7?');
-        $I->see('CentOS is a Linux distribution that provides a free, enterprise-class, community-supported computing platform functionally compatible with its upstream source, Red Hat Enterprise Linux');
-        $I->see('What is Virtual Machine?');
-        $I->see('In computing, a virtual machine is an emulation of a computer system. Virtual machines are based on computer architectures and provide functionality of a physical computer.');
-        $I->see('Why should you use Centos 7 and VirtualBox?');
-        $I->see('Well, they’re free and beginner friendly (I think lol )');
+        $I->click('About');
+        $I->seeCurrentURLEquals('/qaengineers/about');
+        $I->see('About');
+        $I->see('Written by Limuel');
+        $I->see('on June 05, 2018');
+        $I->see('Hi! My name is Limuel. I like coffee, music, games and technology. I’m currently a Lead Software Quality Assurance Engineer based here in the Philippines and I’m very passionate about DevOps, Automation, Software Testing and Web Development.');
     }
 }
